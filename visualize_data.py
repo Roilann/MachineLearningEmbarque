ab3c06b1fbbefea56137145cf6ee0200a771115e
Roilann <86485854+Roilann@users.noreply.github.com>
2023-12-19 10:36:56 +0100

    modification to visualize_data.py to see output files

--- visualize_data.py
+++ visualize_data.py
@@ -2,88 +2,136 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
+import os
 
 
-# Charger le fichier CSV
-df = pd.read_csv('input/chaotique_02.csv')
+def verify_folder_input(user_input_folder):
+    folder_authorized = False
+    output = None
+    if user_input_folder == 'input' or user_input_folder == 'in':
+        output = "input"
+        folder_authorized = True
+    elif user_input_folder == 'output' or user_input_folder == 'out':
+        output = "output"
+        folder_authorized = True
 
-# Assure-toi que les colonnes appropriées sont sélectionnées
-temps = df['T [ms]']
-acceleration_x = df['AccX [mg]']
-acceleration_y = df['AccY [mg]']
-acceleration_z = df['AccZ [mg]']
-gyro_x = df['GyroX [mdps]']
-gyro_y = df['GyroY [mdps]']
-gyro_z = df['GyroZ [mdps]']
-data = df['AccX [mg]'].values
+    return folder_authorized, output
 
-# Calcul de la DFT
-dft_result = np.fft.fft(data)
+def verify_csv_input(user_input_csv, checked_directory):
+    test_path = os.path.join(checked_directory, user_input_csv+'.csv')
+    return os.path.isfile(test_path), test_path
 
-# Calcul de la FFT
-fft_result = np.fft.fft(data)
 
-# Fréquences associées à la transformation
-freq = np.fft.fftfreq(len(data))
+# Create Time columns for visualization
+def add_t_column(csv_df):
+    # Create an incremental 'T [ms]' column with a step size of 20
+    csv_df['T [ms]'] = range(0, 20 * len(df), 20)
+    # Display the DataFrame with the new 'T [ms]' column
+    # print(df)
+    # You can use df.plot() or any other visualization library to plot the data
 
-# Tracé des résultats
-plt.figure(figsize=(12, 6))
 
-plt.subplot(2, 1, 1)
-plt.plot(freq, np.abs(dft_result))
-plt.title('Transformée de Fourier Discrète (DFT)')
-plt.xlabel('Fréquence')
-plt.ylabel('Amplitude')
+input_folder = input("Enter the folder of the csv file :")
+folder_flag, directory = verify_folder_input(input_folder)
+if folder_flag:
+   csv_file = input("Enter the name of the csv file :")
+   csv_flag, csv_path = verify_csv_input(csv_file, directory)
+   if csv_flag:
 
-plt.subplot(2, 1, 2)
-plt.plot(freq, np.abs(fft_result))
-plt.title('Transformée de Fourier Rapide (FFT)')
-plt.xlabel('Fréquence')
-plt.ylabel('Amplitude')
+    # Charger le fichier CSV
+    df = pd.read_csv(csv_path)
+    # Verification de la présence de la colonne temps ou création temporaire
+    try:
+        temps = df['T [ms]']
+    except:
+        add_t_column(df)
+        temps = df['T [ms]']
 
-plt.tight_layout()
-plt.show()
+    # Attribution des colonnes du csv vers dans des variables
+    acceleration_x = df['AccX [mg]']
+    acceleration_y = df['AccY [mg]']
+    acceleration_z = df['AccZ [mg]']
 
-# Plot des données
-plt.figure(figsize=(12, 6))
-plt.plot(temps, acceleration_x, label='Accélération X')
-plt.plot(temps, acceleration_y, label='Accélération Y')
-plt.plot(temps, acceleration_z, label='Accélération Z')
+    if directory == 'input':
+        gyro_x = df['GyroX [mdps]']
+        gyro_y = df['GyroY [mdps]']
+        gyro_z = df['GyroZ [mdps]']
 
-plt.title('Données de l\'accéléromètre au fil du temps')
-plt.xlabel('Temps')
-plt.ylabel('Accélération')
-plt.legend()
-plt.show()
+    data = df['AccX [mg]'].values
 
-# Plot en 3D
-fig = plt.figure(figsize=(10, 8))
-ax = fig.add_subplot(111, projection='3d')
-ax.plot(temps, acceleration_x, acceleration_y, label='Trajectoire 3D de l\'accélération')
-ax.set_xlabel('Temps')
-ax.set_ylabel('Accélération X')
-ax.set_zlabel('Accélération Y')
-ax.legend()
-plt.show()
+    # Calcul de la DFT
+    dft_result = np.fft.fft(data)
 
-# Plot des données
-plt.figure(figsize=(12, 6))
-plt.plot(temps, gyro_x, label='Gyro X')
-plt.plot(temps, gyro_y, label='Gyro Y')
-plt.plot(temps, gyro_z, label='Gyro Z')
+    # Calcul de la FFT
+    fft_result = np.fft.fft(data)
 
-plt.title('Données du gyroscope au fil du temps')
-plt.xlabel('Temps')
-plt.ylabel('Gyro')
-plt.legend()
-plt.show()
+    # Fréquences associées à la transformation
+    freq = np.fft.fftfreq(len(data))
 
-# Plot en 3D
-fig = plt.figure(figsize=(10, 8))
-ax = fig.add_subplot(111, projection='3d')
-ax.plot(temps, gyro_x, gyro_y, label='Trajectoire 3D du gyroscope')
-ax.set_xlabel('Temps')
-ax.set_ylabel('Gyro X')
-ax.set_zlabel('Gyro Y')
-ax.legend()
-plt.show()
+    # Tracé des résultats
+    plt.figure(figsize=(12, 6))
+
+    plt.subplot(2, 1, 1)
+    plt.plot(freq, np.abs(dft_result))
+    plt.title('Transformée de Fourier Discrète (DFT)')
+    plt.xlabel('Fréquence')
+    plt.ylabel('Amplitude')
+
+    plt.subplot(2, 1, 2)
+    plt.plot(freq, np.abs(fft_result))
+    plt.title('Transformée de Fourier Rapide (FFT)')
+    plt.xlabel('Fréquence')
+    plt.ylabel('Amplitude')
+
+    plt.tight_layout()
+    plt.show()
+
+    # Plot des données
+    plt.figure(figsize=(12, 6))
+    plt.plot(temps, acceleration_x, label='Accélération X')
+    plt.plot(temps, acceleration_y, label='Accélération Y')
+    plt.plot(temps, acceleration_z, label='Accélération Z')
+
+    plt.title('Données de l\'accéléromètre au fil du temps')
+    plt.xlabel('Temps')
+    plt.ylabel('Accélération')
+    plt.legend()
+    plt.show()
+
+    # Plot en 3D
+    fig = plt.figure(figsize=(10, 8))
+    ax = fig.add_subplot(111, projection='3d')
+    ax.plot(temps, acceleration_x, acceleration_y, label='Trajectoire 3D de l\'accélération')
+    ax.set_xlabel('Temps')
+    ax.set_ylabel('Accélération X')
+    ax.set_zlabel('Accélération Y')
+    ax.legend()
+    plt.show()
+
+    # Plot des données
+    if directory == 'input':
+        plt.figure(figsize=(12, 6))
+        plt.plot(temps, gyro_x, label='Gyro X')
+        plt.plot(temps, gyro_y, label='Gyro Y')
+        plt.plot(temps, gyro_z, label='Gyro Z')
+
+        plt.title('Données du gyroscope au fil du temps')
+        plt.xlabel('Temps')
+        plt.ylabel('Gyro')
+        plt.legend()
+        plt.show()
+
+        # Plot en 3D
+        fig = plt.figure(figsize=(10, 8))
+        ax = fig.add_subplot(111, projection='3d')
+        ax.plot(temps, gyro_x, gyro_y, label='Trajectoire 3D du gyroscope')
+        ax.set_xlabel('Temps')
+        ax.set_ylabel('Gyro X')
+        ax.set_zlabel('Gyro Y')
+        ax.legend()
+        plt.show()
+
+    else :
+        print("Could not find csv file")
+else:
+    print("Could not find the folder")
