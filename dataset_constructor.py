55bdfa211bd2d0e6c5609b1c4777490db5a1ba3c
Théo FAUCHER <theo.freelance@fchr.fr>
2023-12-19 08:27:06 +0100

    Ajout de l'affichage du dataset en cours de construction
    Signed-off-by: Théo FAUCHER <theo.freelance@fchr.fr>

--- dataset_constructor.py
+++ dataset_constructor.py
@@ -3,6 +3,26 @@ import numpy as np
 import pandas as pd
 from matplotlib import pyplot as plt
 
+
+def print_dataset(dataset, title: str = 'Dataset state'):
+    plt.figure(figsize=(10, 6))
+    plt.subplot(2, 1, 1)
+    plt.plot(dataset['AccX [mg]'], label='AccX as a function of T')
+    plt.plot(dataset['AccY [mg]'], label='AccY as a function of T')
+    plt.plot(dataset['AccZ [mg]'], label='AccZ as a function of T')
+    plt.xlabel('Point number')
+    plt.ylabel('Acceleration')
+    plt.title(title)
+
+    plt.subplot(2, 1, 2)
+    plt.plot(dataset['State'])
+    plt.xlabel('Point number')
+    plt.ylabel('State')
+
+    plt.grid(True)
+    plt.show()
+
+
 files = [f for f in pathlib.Path().glob("./output/*.csv")]
 for index, file in enumerate(files):
     print(f"{index} - {file}")
@@ -20,34 +40,22 @@ while True:
     print(f"File selected: {file_selected}")
 
     data = pd.read_csv(file_selected, usecols=headers)
+    print_dataset(data, 'File selected: ' + file_selected.name)
 
-    # Padding ou troncature des données
-    if data.shape[0] < 100:
-        # Créer un DataFrame de padding avec les mêmes en-têtes
-        padding = pd.DataFrame(np.zeros((100 - data.shape[0], len(headers))), columns=headers)
-        data = pd.concat([data, padding], ignore_index=True)
-    elif data.shape[0] > 100:
-        data = data.iloc[:100]
+    action = input("Do you want to add this values in the dataset (Y/n): ")
+    if action.lower() == 'y' or action.lower() == 'yes':
+        # Padding ou troncature des données
+        if data.shape[0] < 100:
+            # Créer un DataFrame de padding avec les mêmes en-têtes
+            padding = pd.DataFrame(np.zeros((100 - data.shape[0], len(headers))), columns=headers)
+            data = pd.concat([data, padding], ignore_index=True)
+        elif data.shape[0] > 100:
+            data = data.iloc[:100]
 
-    dataset = pd.concat([dataset, data], ignore_index=True)
+        dataset = pd.concat([dataset, data], ignore_index=True)
+        print_dataset(dataset, 'Actual dataset')
 
-plt.figure(figsize=(10, 6))
-plt.subplot(2, 1, 1)
-plt.plot(dataset['AccX [mg]'], label='AccX as a function of T')
-plt.plot(dataset['AccY [mg]'], label='AccY as a function of T')
-plt.plot(dataset['AccZ [mg]'], label='AccZ as a function of T')
-plt.xlabel('Point number')
-plt.ylabel('Acceleration')
-plt.title('Dataset')
-
-plt.subplot(2, 1, 2)
-plt.plot(dataset['State'])
-plt.xlabel('Point number')
-plt.ylabel('State')
-plt.title('Dataset state')
-
-plt.grid(True)
-plt.show()
+print_dataset(dataset)
 
 dataset_name = input("Enter the name of the dataset to export: ")
 dataset.to_csv(f'datasets/{dataset_name}.csv', index=False, header=True)
