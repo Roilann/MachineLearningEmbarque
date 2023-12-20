9dd4f698577df6bc7ddb4e13e7c82f67b5f5a004
Roilann <86485854+Roilann@users.noreply.github.com>
2023-12-20 10:04:41 +0100

    macro to easily change data_point analysis

--- model.py
+++ model.py
@@ -7,6 +7,9 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import MinMaxScaler
 
 
+DATA_POINTS = 100
+
+
 def scale_sequences(sequences):
     scaler = MinMaxScaler(feature_range=(0, 1))
     scaler.fit_transform(sequences)
@@ -17,21 +20,21 @@ seed = int(time.time())
 np.random.seed(seed)
 tf.random.set_seed(seed)
 
-raw_dataset = pd.read_csv('dataset.csv').values
+raw_dataset = pd.read_csv('datasets/dataset.csv').values
 
 E_raw_dataset = raw_dataset[:, :-1]
 Y_raw_dataset = raw_dataset[:, -1]
 
 # Vérification si la longueur de raw_dataset est un multiple de 200
-if len(raw_dataset) % 100 != 0:
+if len(raw_dataset) % DATA_POINTS != 0:
     raise ValueError("Le nombre de lignes dans le fichier CSV n'est pas un multiple de 200.")
 
 scaler = MinMaxScaler(feature_range=(0, 1))
 E_raw_dataset = scaler.fit_transform(E_raw_dataset)
 
 # Division du dataset en sous-tableaux
-E_dataset = np.array_split(E_raw_dataset, len(raw_dataset) // 100)
-Y_dataset = np.array_split(Y_raw_dataset, len(raw_dataset) // 100)
+E_dataset = np.array_split(E_raw_dataset, len(raw_dataset) // DATA_POINTS)
+Y_dataset = np.array_split(Y_raw_dataset, len(raw_dataset) // DATA_POINTS)
 
 # Vérifier que toutes les étiquettes sont identiques dans chaque lot
 for lot in Y_dataset:
@@ -48,7 +51,7 @@ Y_dataset = np.asarray(Y_dataset)
 E_train_dataset, E_test_dataset, Y_train_dataset, Y_test_dataset = train_test_split(E_dataset, Y_dataset, test_size=0.2,
                                                                                     random_state=seed)
 model = tf.keras.models.Sequential([
-    tf.keras.layers.Dense(100, activation='relu', input_shape=(E_train_dataset.shape[1], E_train_dataset.shape[2])),
+    tf.keras.layers.Dense(DATA_POINTS, activation='relu', input_shape=(E_train_dataset.shape[1], E_train_dataset.shape[2])),
     tf.keras.layers.Dropout(0.2),
     tf.keras.layers.Dense(50, activation='relu'),
     tf.keras.layers.Dropout(0.2),
